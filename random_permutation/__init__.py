264348eb2c2b4090e4d0d1d2019df236bd61d7c4
Gary William Flake <gary@flake.org>
2023-10-03 11:28:34 -0700

    pylint workflow

--- random_permutation/__init__.py
+++ random_permutation/__init__.py
@@ -1,1 +1,7 @@
+"""
+This module provides the RandomPermutation class for efficiently
+generating very large random permutations of a given size.  The
+permutations can be varied using the seed parameter and the number of
+ciphers used can be controlled using the num_ciphers parameter.
+"""
 from .permutation import RandomPermutation
