48d5ef24bca088e22c26a950fd95044e43bb59e6
Gary William Flake <gary@flake.org>
2023-10-04 11:48:09 -0700

    encoding detail in setup

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
   author_email='gary@flake.org',
   description=
   'A module for efficiently generating very large random permutations',
-  long_description=open('README.md').read(),
+  long_description=open('README.md', encoding='utf-8').read(),
   long_description_content_type='text/markdown',
   packages=find_packages(
     include=['random_permutation', 'random_permutation.*']),
