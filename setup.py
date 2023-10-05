bd0ae890eb6652a6d3bf2250905f7a1276e08f4c
Gary William Flake <gary@flake.org>
2023-10-04 17:18:33 -0700

    updated version to 0.1.11

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
   name='random_permutation',
-  version='0.1.0',
+  version='0.1.11',
   author='Gary William Flake',
   author_email='gary@flake.org',
   description=
